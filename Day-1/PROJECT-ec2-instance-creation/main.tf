ac3e1cfb416616ea2dc81e99189cb12a8e279e0b
Nishy49 <nishyfishy49@gmail.com>
2025-06-28 22:09:30 +0530

    added new files

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,11 @@
+
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1" #region is set to mumbai
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "Demo1" {
+    ami = "ami-020cba7c55df1f615"
     instance_type = "t2.micro"
+    subnet_id = "subnet-096ae5a304df64b07"
+    key_name = "terraform-demo-user"
 }
